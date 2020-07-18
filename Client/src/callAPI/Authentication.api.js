33eb018075efd998c550d16f25d2d27f40e7e452
truong8864 <nhom1hd1@gmail.com>
2020-07-18 14:27:36 +0700

    ok

---
+++ Client/src/callAPI/Authentication.api.js
@@ -0,0 +1,27 @@
+import clientAPI from "./Client.api";
+
+export const login = (params) => {
+  const url = "/login";
+  return clientAPI.post(url, params);
+};
+
+// class AuthenticationAPI {
+//   login = (params) => {
+//     const url = "/login";
+//     return clientAPI.post(url, params);
+//   };
+
+//   logout = (params) => {
+//     const url = "/logout";
+//     return clientAPI.get(url, params);
+//   };
+
+//   checkLogin = (params) => {
+//     const url = "/check-login";
+//     return clientAPI.get(url, params);
+//   };
+// }
+
+// const authenticationAPI = new AuthenticationAPI();
+
+// export default authenticationAPI;
