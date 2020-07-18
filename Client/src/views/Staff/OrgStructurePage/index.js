533027b0fdfb930720e317e580830daeb42f17cd
truong8864 <nhom1hd1@gmail.com>
2020-07-18 12:41:50 +0700

    add

--- Client/src/views/Staff/OrgStructurePage/index.js
+++ Client/src/views/Staff/OrgStructurePage/index.js
@@ -14,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(1),
-    textAlign: "center",
+    //textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
