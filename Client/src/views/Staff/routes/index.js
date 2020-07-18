42e45f7438437ddaec9bd2025cccfbce3e801222
ThachTua <thachtua66@gmail.com>
2020-07-18 13:32:54 +0700

    new1

--- Client/src/views/Staff/routes/index.js
+++ Client/src/views/Staff/routes/index.js
@@ -3,8 +3,8 @@ import React from "react";
 const OrgStructurePage = React.lazy(() => import("../OrgStructurePage"));
 
 const staffRoute = [
-  { path: "/nhan-su/", name: "Nhân sự", component: OrgStructurePage, exact: true},
-  { path: "/nhan-su/to-chu-nhan-su", name: "Tổ chức nhân sự",component: OrgStructurePage},
+  { path: "/nhan-su", name: "Nhân sự", exact: true, component: OrgStructurePage},
+  { path: "/nhan-su/to-chuc-nhan-su", name: "Tổ chức nhân sự",component: OrgStructurePage},
 ];
 
 export default staffRoute;
