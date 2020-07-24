583fd87259f761c70f82ff0a8ce02da639417a8a
ThachTua <thachtua66@gmail.com>
2020-07-24 15:16:18 +0700

    abc

--- Client/src/callAPI/Authentication.api.js
+++ Client/src/callAPI/Authentication.api.js
@@ -1,9 +1,11 @@
 import callAPI from "./callAPI";
 
-export default function LoginAPI  (body)  {
+export function LoginAPI  (body)  {
   return callAPI("/user/login", "POST", body);
 };
-
+export function RegisterApi  (body)  {
+  return callAPI("/user/register", "POST", body);
+};
 // HAHA class AuthenticationAPI {
 //   login = (params) => {
 //     const url = "/login";
--- Client/src/views/pages/register/Register.js
+++ Client/src/views/pages/register/Register.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react'
+import qs from 'qs'
 import {
   CButton,
   CCardBody,
@@ -11,34 +12,103 @@ import {
 
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import { makeStyles } from '@material-ui/core/styles';
+import {RegisterApi} from '../../../callAPI/Authentication.api'
+import Alert from '@material-ui/lab/Alert';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 const Register = () => {
+  const [name,setname]=useState("");
+  const [pass,setPass]=useState("");
+  const [rePass,setRePass]=useState("");
+  const [mss, setMss]=useState("")
+  const [err,setErr]=useState("");
+  const [hide,setHide]=useState(false);
+
+  const up_Name = (e) =>
+  {
+    setname(e.target.value)
+  }
+  const up_Pass = (e) =>
+  {
+    setPass(e.target.value)
+  }
+  const up_RePass = (e) =>
+  {
+    setRePass(e.target.value)
+  }
+  const On_Register = ()=>{
+    setHide(true)
+    if(name===""||pass===""||rePass==="")
+    {
+      return setErr("Bắt buộc phải nhập");
+    }
+    else{
+      RegisterApi(qs.stringify({
+        role:"admin",
+        username:name,
+        password:pass,
+        password_confirm:rePass
+      })).then(res=>{
+        if(res.data)
+        {
+          if(res.data.mss)
+          {
+            setMss(res.data.mss)
+          }
+          if(res.data.err)
+          {
+            setErr(res.data.err)
+          }
+        }
+      })
+    }
+  }
   return (
       <CContainer>
               <CCardBody className="p-4">
                 <CForm>
                   <h1>Tạo tài khoản</h1>
+                { hide===true? ( (err !== "" && mss==="") ?
+                  <div className={useStyles.root}>
+                    <Alert severity="error">{err} - check it out</Alert>
+                  </div>: <Alert severity="success">{mss} — success!</Alert>
+                 ):""
+                }
+
                   <CInputGroup className="mb-3">
                     <CInputGroupPrepend>
                       <CInputGroupText>
                         <CIcon name="cil-user" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="text" placeholder="Username" autoComplete="username" />
+                    <CInput onChange={up_Name} type="text" placeholder="Username" autoComplete="username" />
                   </CInputGroup>
-                  <CInputGroup className="mb-3">
+                  {
+                    /*
+                    <CInputGroup className="mb-3">
                     <CInputGroupPrepend>
                       <CInputGroupText>@</CInputGroupText>
                     </CInputGroupPrepend>
                     <CInput type="text" placeholder="Email" autoComplete="email" />
                   </CInputGroup>
+                    */
+                  }
+
                   <CInputGroup className="mb-3">
                     <CInputGroupPrepend>
                       <CInputGroupText>
                         <CIcon name="cil-lock-locked" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="password" placeholder="Password" autoComplete="new-password" />
+                    <CInput onChange={up_Pass} type="password" placeholder="Password" autoComplete="new-password" />
                   </CInputGroup>
                   <CInputGroup className="mb-4">
                     <CInputGroupPrepend>
@@ -46,9 +116,9 @@ const Register = () => {
                         <CIcon name="cil-lock-locked" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="password" placeholder="Repeat password" autoComplete="new-password" />
+                    <CInput onChange={up_RePass} type="password" placeholder="Repeat password" autoComplete="new-password" />
                   </CInputGroup>
-                  <CButton color="success" block>Create Account</CButton>
+                  <CButton onClick={On_Register} color="success" block>Create Account</CButton>
                 </CForm>
               </CCardBody>
               {/*
