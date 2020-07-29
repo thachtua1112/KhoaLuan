9b0c2a02991cd3bbd1b58301ce2f4708534151f9
truong8864 <nhom1hd1@gmail.com>
2020-07-29 17:09:32 +0700

    ok

---
+++ Server/api/v1/models/Att_TimeKeepingGroup.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Att_TimeKeepingGroupSchema = new Schema({
+  KiCong: { type: Schema.Types.String, required },
+  ProfileID: { type: Schema.Types.String, required },
+  UnSabbaticalLeave: { type: Schema.Types.Number },
+  SabbaticalLeave: { type: Schema.Types.Number },
+  TotalKeepingReality: { type: Schema.Types.Number },
+  SumKeeping: { type: Schema.Types.Number },
+  Description: { type: Schema.Types.String },
+});
+
+const Att_TimeKeepingGroupModel = mongoose.model(
+  "Att_TimeKeepingGroup",
+  Att_TimeKeepingGroupSchema
+);
+
+module.exports = Att_TimeKeepingGroupModel;
