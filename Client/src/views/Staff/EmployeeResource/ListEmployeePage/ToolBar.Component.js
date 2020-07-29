0ac42afd44b404e3c0056d6833cd1d45fa7aec37
truong8864 <nhom1hd1@gmail.com>
2020-07-29 15:03:15 +0700

    ok

--- Client/src/views/Staff/EmployeeResource/ListEmployeePage/ToolBar.Component.js
+++ Client/src/views/Staff/EmployeeResource/ListEmployeePage/ToolBar.Component.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core/styles";
-
 import { Toolbar, Tooltip, IconButton, Button } from "@material-ui/core";
 
 import CreateIcon from "@material-ui/icons/Create";
