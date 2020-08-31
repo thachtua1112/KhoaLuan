e44e9e246e2683adfa984f14fd55cc270bb0cfe6
truong <nhom1hd1@gmail.com>
2020-08-31 10:53:21 +0700

    ok

--- Server/src/api/v1/routes/index.js
+++ Server/src/api/v1/routes/index.js
@@ -5,17 +5,31 @@ const RoleRoute = require("../Role/Role.route");
 const ProfileRoute = require("../HumanResourceExecutive/Profile/Hre_Profile.route");
 const ContractRoute = require("../HumanResourceExecutive/Contract/Hre_Contract.route");
 const StopWorkingRoute = require("../HumanResourceExecutive/StopWorking/Hre_StopWorking.route");
+
 const OrgStructureRoute = require("../Category/OrgStructure/Cat_OrgStructure.route");
 const OrgStructureTreeRoute = require("../Category/OrgStructureTree/Cat_OrgStructureTree.route");
+const PositionRoute = require("../Category/Position/Cat_Position.route");
 
 const RouterV1 = express.Router();
 
+/**
+ * System
+ */
 RouterV1.use("/users", UserRoute);
 RouterV1.use("/roles", RoleRoute);
+
+/**
+ * Hre
+ */
 RouterV1.use("/profiles", ProfileRoute);
 RouterV1.use("/contracts", ContractRoute);
 RouterV1.use("/stop-workings", StopWorkingRoute);
+
+/**
+ * Cat
+ */
 RouterV1.use("/org-structures", OrgStructureRoute);
 RouterV1.use("/org-structures-tree", OrgStructureTreeRoute);
+RouterV1.use("/positions", PositionRoute);
 
 module.exports = RouterV1;
